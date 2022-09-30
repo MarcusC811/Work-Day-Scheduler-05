7147db9f6fae0141d3ee1e89563b9eaa0a20abfd
OVOMARK <mcncstatefan@icloud.com>
2022-09-29 23:00:03 -0400

    adding logic for color time tables

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
+var textBox = document.getElementsByTagName("textarea");
+var timeBox = document.getElementsByClassName("time-field");
+var testing = document.getElementById("test");
 var today = moment();
 $("#currentDay").text(today.format("LLLL"));
+
+console.log(timeBox.textContent);
+
+if(moment().format('h') === timeBox.text) {
+
+} 
