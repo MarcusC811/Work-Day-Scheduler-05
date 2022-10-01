ca1ba0a1b2ae2a760ca5d09a8d3f32417fb036d4
OVOMARK <mcncstatefan@icloud.com>
2022-10-01 17:10:14 -0400

    almost working time slots color cord.

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,22 @@ var testing = document.getElementById("test");
 var today = moment();
 $("#currentDay").text(today.format("LLLL"));
 
-console.log(timeBox.textContent);
 
-if(moment().format('h') === timeBox.text) {
 
-} 
+for(i=0; i<textBox.length; i++) {
+    if(textBox[i].id < moment().format('HH')) {
+        $(textBox[i]).addClass("past")
+        $(textBox[i]).removeClass("future")
+        $(textBox[i]).removeClass("present")
+    } else if(textBox[i].id === moment().format('HH')) {
+        $(textBox[i]).addClass("present")
+        $(textBox[i]).removeClass("future")
+        $(textBox[i]).removeClass("past")
+    } else if(textBox[i].id > moment().format('HH')) {
+        $(textBox[i]).addClass("future")
+        $(textBox[i]).removeClass("present")
+        $(textBox[i]).removeClass("past")
+    }
+}
+
+console.log(textBox)
